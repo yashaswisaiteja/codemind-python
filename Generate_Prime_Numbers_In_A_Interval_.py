3c318b49400218eeb1f2d35ccf29a5fa88e56e17
yashaswisaiteja <119913198+yashaswisaiteja@users.noreply.github.com>
2023-05-23 21:25:26 +0530

    a=int(input()) b=int(input()) for n in range(a,b+1):     if n>1:         for i in range(2,n):             if(n%i)==0:                 break         else:             print(n)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,6 +1,6 @@
-low=int(input())
-upp=int(input())
-for n in range(low,upp+1):
+a=int(input())
+b=int(input())
+for n in range(a,b+1):
     if n>1:
         for i in range(2,n):
             if(n%i)==0:
